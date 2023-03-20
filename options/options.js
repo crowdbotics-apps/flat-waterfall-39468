5f64f40aaf0b525d7ffc6ef649bc9a6e6d7abf88
Crowdbotics <team@crowdbotics.com>
2023-03-20 20:04:23 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "flat-waterfall-39468",
+  url: "https://flat-waterfall-39468.botics.co",
+  api: "https://flat-waterfall-39468.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
